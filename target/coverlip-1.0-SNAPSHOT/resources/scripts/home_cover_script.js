293c1b17c11ce98907bf8d6376f65b8f4faa036e
tonquangtu <tonquangtu49@gmail.com>
2017-05-21 17:32:37 +0700

    s

--- target/coverlip-1.0-SNAPSHOT/resources/scripts/home_cover_script.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nguyenthanhtung on 07/05/2017.
- */
-$(document).ready(function(){
-    var animate = true;
-    $('.new_list_content .thumbnail_video_box').append('<span class="barge" style="background-image: url(http://static.mp3.zdn.vn/skins/zmp3-v4.2/images/badge.png);background-repeat: no-repeat;position: absolute;top: 0;left:0.5px;display: block;background-size: 200%;width: 45px;height: 45px;"></span>');
-    $('.hot_list_content .thumbnail_video_box').append('<span class="barge" style="background-image: url(http://static.mp3.zdn.vn/skins/zmp3-v4.2/images/badge.png);background-repeat: no-repeat;position: absolute;top: 0;left:0.5px;display: block;background-size: 200%;width: 45px;height: 45px;background-position: -45px 0;"></span>');
-});
